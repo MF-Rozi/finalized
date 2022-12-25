0f3fccc18eea9df5835a4e23fc96258bfe2a5639
mfrozi <m.fakhrulrozi10@gmail.com>
2022-12-25 15:27:40 +0700

    create post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $name = fake()->sentence(2);
+        return [
+            'title'=>$name,
+            'content'=>fake()->text(1000),
+            'category_id'=> fake()->numberBetween(1,Category::count()),
+            'user_id'=> fake()->numberBetween(1,User::count())
+        ];
+    }
+    public function setUser(User $user){
+        $name = fake()->sentence(2);
+        return $this->state(fn (array $attributes) => [
+            'title'=>$name,
+            'content'=>fake()->text(1000),
+            'category_id'=> fake()->numberBetween(1,Category::count()),
+            'user_id'=> $user->id,
+        ]);
+    }
+}
