6f8afcd207ae15a1cb86e34cc47c3e69a3988413
mfrozi <m.fakhrulrozi10@gmail.com>
2022-12-26 16:07:21 +0700

    tried to use datatable

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            'bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            'datatable': path.resolve(__dirname, 'node_modules/datatable'),
+            'jquery': path.resolve(__dirname, 'node_modules/jquery'),
+            'lodash': path.resolve(__dirname, 'node_modules/lodash'),
+            'axios': path.resolve(__dirname, 'node_modules/axios'),
+            'popper': path.resolve(__dirname, 'node_modules/@popperjs/core'),
+
+        }
+    },
 });
